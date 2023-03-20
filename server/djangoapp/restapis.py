b2bc6867326ba5d45b917cabdcc2ab621bc43511
AparnaHalder <aparna.halder82@gmail.com>
2023-03-20 12:23:53 -0400

    Changes made from Thea Lab

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -9,31 +9,19 @@ from requests.auth import HTTPBasicAuth
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
-
-    # If argument contain API KEY
-    api_key = kwargs.get("api_key")
+    print(kwargs)
     print("GET from {} ".format(url))
     try:
-        if api_key:
-            params = dict()
-            params["text"] = kwargs["text"]
-            params["version"] = kwargs["version"]
-            params["features"] = kwargs["features"]
-            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
-            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-                                    auth=HTTPBasicAuth('api_key', api_key))
-        else:
-            # Call get method of requests library with URL and parameters
-            response = requests.get(url, headers={'Content-Type': 'application/json'},
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
                                     params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
-
     status_code = response.status_code
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
-    return json_data
+    return json_data    
 
 
 # Create a `post_request` to make HTTP POST requests
@@ -47,15 +35,7 @@ def get_request(url, **kwargs):
 def get_dealers_from_cf(url, **kwargs):
     results = []
     # Call get_request with a URL parameter
-    #json_result = get_request(url)
-    state = kwargs.get("state")
-    if state:
-        json_result = get_request(url, state=state)
-    else:
-        json_result = get_request(url)
-
-    print('json_result from line 31', json_result)    
-    
+    json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
         #dealers = json_result["rows"]
@@ -70,7 +50,6 @@ def get_dealers_from_cf(url, **kwargs):
                                    short_name=dealer_doc["short_name"],
                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
             results.append(dealer_obj)
-
     return results
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
